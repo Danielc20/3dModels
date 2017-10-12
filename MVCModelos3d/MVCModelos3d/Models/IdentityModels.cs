a03d5f98be5ca3dd52cc55006960ac56ba0595d4
Daniel <danielcdbc18@gmail.com>
2017-10-12 18:33:51 -0200

    Added 'Modelo3d' Model and User Modelo3d collection

--- MVCModelos3d/MVCModelos3d/Models/IdentityModels.cs
+++ MVCModelos3d/MVCModelos3d/Models/IdentityModels.cs
@@ -3,12 +3,17 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using MVCModelos3d.Models.Modelos3d;
+using System.Collections.Generic;
 
 namespace MVCModelos3d.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+
+        public virtual List<Modelo3d> misModelos3d { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -16,10 +21,18 @@ namespace MVCModelos3d.Models
             // Add custom user claims here
             return userIdentity;
         }
+
+        public ApplicationUser getCurrentUser(ApplicationDbContext db, string usrId)
+        {
+            return db.Users.Find(usrId);
+        }
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+
+        DbSet<Modelo3d> colModelos3d { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
---
+++ MVCModelos3d/MVCModelos3d/Models/Modelos3d/Modelo3d.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCModelos3d.Models.Modelos3d
+{
+    public class Modelo3d
+    {
+        [Display(Name = "Titulo del Modelo 3D")]
+        public string Titulo { get; set; }
+        [Display(Name = "Descripcción")]
+        public string Descripcion { get; set; }
+        [Display(Name = "Precio del Modelo 3D", Description = "Precio del Modelo en Dolares")]
+        [DataType(DataType.Currency)]
+        public float Precio { get; set; }
+
+        public ApplicationUser userObj { get; set; }
+
+        //prop Foto del Modelo3d
+    }
+}
